d59cb19614d4301012e7842aea8c148aa068487f
Sam <loboabel.cronox@gmail.com>
2024-02-01 18:37:20 -0600

    Se creo el moduclo 1 y se provo con exito

--- resgistro_user.py
+++ resgistro_user.py
@@ -1,1 +1,8 @@
 #Sub modulo 1 entrada de datos y validacion de usuario
+print("Welcomo to Sing Up")
+newUser=input("Pleace write a new user name: ")
+dataBase="Fulanito"
+if newUser != dataBase:
+    print(F"El nombre de usuario {newUser} Esta disponible");
+else:
+    print(f"Lo siento el nombre {newUser} No esta disponible")
