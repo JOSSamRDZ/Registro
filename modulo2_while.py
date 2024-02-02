4029a23b5b544c3ce652971d556619f5a881d46b
Sam <loboabel.cronox@gmail.com>
2024-02-01 19:18:55 -0600

    Se creo el modulo 2 y submodulo 2.1 hasta ahora funciona la validacion del primero y la entrada de datos del segundo, ademas se creo un archivo .py aparte para provar manejar los intentos de validar la contrasena

---
+++ modulo2_while.py
@@ -0,0 +1,5 @@
+user_pass=input("Crea una nueva 8 caracteres maximo\ncontrasena: ")
+while len [user_pass] < 8:
+    
+else:
+    
--- resgistro_user.py
+++ resgistro_user.py
@@ -3,6 +3,15 @@ print("Welcomo to Sing Up")
 newUser=input("Pleace write a new user name: ")
 dataBase="Fulanito"
 if newUser != dataBase:
-    print(F"El nombre de usuario {newUser} Esta disponible");
+    print(F"El nombre de usuario {newUser} Esta disponible")
+    #Modulo 2 validacion de contraseña
+    user_pass=input("Crea una nueva 8 caracteres maximo\ncontrasena: ")
+    if len [user_pass] < 8:
+        #Submodulo 2.1 segunda validacion de la contrsena
+        print("Por favor vuelva esvrivir la contrasna:")
+        
+    else:
+        print("lo siento la contrasena supera los 8 caracteres")
+   
 else:
     print(f"Lo siento el nombre {newUser} No esta disponible")
